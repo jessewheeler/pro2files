f1000eb07aa6d0fae435b2b93e086df9750364e9
Jesse Wheeler <jesse@wheeler.id>
2022-12-06 13:58:02 -0700

    fix limit bug, only process object perms of profiles & change profile headers to field labels

--- scripts/profile_report_generator.rb
+++ scripts/profile_report_generator.rb
@@ -5,6 +5,7 @@ require "csv"
 ########################################################################################
 # Set these variables
 ########################################################################################
+
 credentials_path                 = "./environments/sandboxes/iiq-rtsc.json"
 profile_report_output            = "./output/profiles.csv"
 object_permissions_report_output = "./output/object_permissions.csv"
@@ -26,22 +27,22 @@ client = Restforce.new(
     host:           credentials["host"],
 )
 
-fields = []
-
+# Get all profile fields and store as hash of field api name => field label
+fields = {}
 description = client.describe('Profile')
 description.fields.each do |field|
-    fields.push(field.name)
+    fields[field.name] = field.label
 end
 
-query  = 'SELECT ' + fields.join(',') + ' FROM Profile'
-profiles = client.query('SELECT ' + fields.join(', ') + ' FROM Profile')
+query  = 'SELECT ' + fields.keys.join(',') + ' FROM Profile'
+profiles = client.query(query)
 
 CSV.open(profile_report_output, "wb") do |writer|
-    writer << fields
+    writer << fields.values # output field labels
     profiles.each do |p|
         values = [];
-        fields.each do |field|
-            values.push(p[field])
+        fields.each do |field, label|
+            values.push(p[field]) # output field api name values
         end
         writer << values
     end
@@ -49,20 +50,22 @@ end
 
 
 fields = ["Id", "Parent.Profile.Name", "SobjectType", "PermissionsCreate", "PermissionsDelete", "PermissionsEdit", "PermissionsModifyAllRecords", "PermissionsRead", "PermissionsViewAllRecords"]
-query  = 'SELECT ' + fields.join(', ') + ' FROM ObjectPermissions LIMIT 1'
+query  = 'SELECT ' + fields.join(', ') + ' FROM ObjectPermissions'
 sObject_permissions = client.query(query);
 
 CSV.open(object_permissions_report_output, "wb") do |writer|
     writer << fields
     sObject_permissions.each do |permission|
-        values = [];
-        fields.each do |field|
-            if field == "Parent.Profile.Name"
-                values.push(permission.Parent.Profile["Name"])
-            else
-                values.push(permission[field])
+        if permission.Parent.Profile != nil
+            values = [];
+            fields.each do |field|
+                if field == "Parent.Profile.Name"
+                    values.push(permission.Parent.Profile["Name"])
+                else
+                    values.push(permission[field])
+                end
             end
+            writer << values
         end
-        writer << values
     end
 end
